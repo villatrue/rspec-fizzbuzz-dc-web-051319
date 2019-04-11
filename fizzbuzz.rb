3151cfbac295c48a8e1ac42409726855444e438b
Sean Carter <scarter0313@gmail.com>
2019-04-11 03:52:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz" # Go fizz
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+  elsif int % 3
+    "Fizz"
   end
 end
